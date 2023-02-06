c03fe8fc096d90cae2d9146e2627f6829da978c4
Glimmer5 <yu_shimizu2@icloud.com>
2023-02-06 12:31:04 +0900

    [add]title

--- src/pages/access.tsx
+++ src/pages/access.tsx
@@ -1,11 +1,19 @@
 import { Container, Box } from "@chakra-ui/react";
-import BreadCrumbButton from '../components/breadCrumbButton';
+import BreadCrumbButton from "../components/breadCrumbButton";
+import Iframe from "../components/iframe";
+import Title from "../components/title";
 
 const Access: React.FC = () => {
   return (
     <Container mb={6} p={3}>
       <Box my={80}>
-        <BreadCrumbButton>Access</BreadCrumbButton>
+        <Box mb={10}>
+          <BreadCrumbButton>Access</BreadCrumbButton>
+        </Box>
+        <Box mb={10}>
+          <Iframe />
+        </Box>
+        <Title>店舗情報</Title>
       </Box>
     </Container>
   );
