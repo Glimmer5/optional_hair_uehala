85c32f9bffdcce85e26e8c29f9df7670d5183347
Glimmer5 <yu_shimizu2@icloud.com>
2023-02-09 16:28:49 +0900

    [add]motionLayout

--- src/pages/access.tsx
+++ src/pages/access.tsx
@@ -5,46 +5,52 @@ import Iframe from "../components/iframe";
 import Title from "../components/title";
 import ArticleBox from "../components/articleBox";
 import LinkButton from "../components/linkButton";
+import MotionLayout from "../components/motionLayout";
+import Section from "../components/section";
 
 const Access: React.FC = () => {
   return (
-    <Container mb={6} p={3}>
-      <Box mt="70px">
-        <Box mb={10}>
-          <BreadCrumbButton>Access</BreadCrumbButton>
-        </Box>
-        <Box mb={10}>
-          <Iframe
-            title="googleMap"
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3199.068505054597!2d138.31431651555206!3d36.696891679969966!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x601d895ee28349c3%3A0x6dc4e3b22b2c3850!2sUehala%20Optional%20Hair!5e0!3m2!1sja!2sjp!4v1649639613112!5m2!1sja!2sjp"
-          />
-        </Box>
-        <Box mb={3}>
-          <Title>店舗情報</Title>
-        </Box>
-        <Box mb={10}>
-          <ArticleBox>
-            <Box>
-              <Text>営業時間／9:00~18:30</Text>
-              <Divider my={2} size="xl" />
-              <Text>定休日／毎週月曜日、第１火曜日、第３日曜日</Text>
-              <Divider my={2} size="xl" />
-              <Text>駐車場／あり(最大３台)</Text>
-              <Divider my={2} size="xl" />
+    <MotionLayout>
+      <Container mb={6} p={3}>
+        <Box mt="70px">
+          <Box mb={10}>
+            <BreadCrumbButton>Access</BreadCrumbButton>
+          </Box>
+          <Box mb={10}>
+            <Iframe
+              title="googleMap"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3199.068505054597!2d138.31431651555206!3d36.696891679969966!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x601d895ee28349c3%3A0x6dc4e3b22b2c3850!2sUehala%20Optional%20Hair!5e0!3m2!1sja!2sjp!4v1649639613112!5m2!1sja!2sjp"
+            />
+          </Box>
+          <Section delay={"0.2"}>
+            <Box mb={3}>
+              <Title>店舗情報</Title>
             </Box>
-            <Box>
-              <Text>〒381-0201</Text>
-              <Text>長野県上高井郡小布施町773</Text>
+            <Box mb={10}>
+              <ArticleBox>
+                <Box>
+                  <Text>営業時間／9:00~18:30</Text>
+                  <Divider my={2} size="xl" />
+                  <Text>定休日／毎週月曜日、第１火曜日、第３日曜日</Text>
+                  <Divider my={2} size="xl" />
+                  <Text>駐車場／あり(最大３台)</Text>
+                  <Divider my={2} size="xl" />
+                </Box>
+                <Box>
+                  <Text>〒381-0201</Text>
+                  <Text>長野県上高井郡小布施町773</Text>
+                </Box>
+              </ArticleBox>
             </Box>
-          </ArticleBox>
+            <Box textAlign="center">
+              <LinkButton href="/" leftIcon={<PhoneIcon />}>
+                026-247-2540
+              </LinkButton>
+            </Box>
+          </Section>
         </Box>
-        <Box textAlign="center">
-          <LinkButton href="/" leftIcon={<PhoneIcon />}>
-            026-247-2540
-          </LinkButton>
-        </Box>
-      </Box>
-    </Container>
+      </Container>
+    </MotionLayout>
   );
 };
 
