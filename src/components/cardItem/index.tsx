9682c010284cc4375762408c4f730bdf1adf35aa
Glimmer5 <yu_shimizu2@icloud.com>
2023-02-13 14:18:16 +0900

    [update]cardItem

--- src/components/cardItem/index.tsx
+++ src/components/cardItem/index.tsx
@@ -1,4 +1,13 @@
-import { Card, CardBody, Heading, Box } from "@chakra-ui/react";
+import {
+  Card,
+  CardBody,
+  CardFooter,
+  Heading,
+  Stack,
+  Box,
+  Text,
+  Divider,
+} from "@chakra-ui/react";
 import Image from "next/image";
 import imageSrc from "../../public/images/menuImage/menu-1.jpeg";
 
@@ -6,13 +15,36 @@ const CardItem = () => {
   return (
     <Card maxW="sm">
       <CardBody>
-        <Box borderRadius="lg">
-          <Image loader={({ src }) => src} alt="cutMenu" src={imageSrc} />
-        </Box>
-        <Heading size="md" textAlign="center">
-          Cut
-        </Heading>
+        <Image
+          loader={({ src }) => src}
+          alt="cutMenu"
+          src={imageSrc}
+          className="card-item-thumbnail"
+        />
+        <Stack mt={6} spacing={3}>
+          <Heading size="md">Cut</Heading>
+          <Box display="flex" justifyContent="space-between">
+            <Text>ハイスタンダードコース</Text>
+            <Text>6,500円</Text>
+          </Box>
+          <Box display="flex" justifyContent="space-between">
+            <Text>クラシックカットコース</Text>
+            <Text>4,500円</Text>
+          </Box>
+          <Box display="flex" justifyContent="space-between">
+            <Text>高校生カットコース</Text>
+            <Text>4,000円</Text>
+          </Box>
+          <Box display="flex" justifyContent="space-between">
+            <Text>高校生エステ無しコース</Text>
+            <Text>3,500円</Text>
+          </Box>
+        </Stack>
+        <Divider mt={5} size="xl" />
       </CardBody>
+      <CardFooter>
+        <Text color="gray.500" fontSize="sm">※シャンプー、ブロー込み</Text>
+      </CardFooter>
     </Card>
   );
 };
