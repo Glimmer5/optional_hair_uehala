579fafb6fdcc91ebc29e87cc2f3c9a3b1a460ac6
Glimmer5 <yu_shimizu2@icloud.com>
2023-02-21 14:08:24 +0900

    [update]cardItems

--- src/components/cardItems/index.tsx
+++ src/components/cardItems/index.tsx
@@ -12,19 +12,25 @@ import {
 import Image from "next/image";
 import cutImage from "../../public/images/menuImage/menu-1.jpeg";
 import colorImage from "../../public/images/menuImage/menu-2.jpeg";
+import permImage from "../../public/images/menuImage/menu-3.jpeg";
+import headspaImage from "../../public/images/menuImage/menu-4.jpeg";
+import shavingImage from "../../public/images/menuImage/menu-5.jpeg";
+import otherImage from "../../public/images/menuImage/menu-6.jpeg";
+
 
 const CardItems = () => {
+
   type cardListProps = {
     id: number;
     title: string;
     nameA: string;
     nameB: string;
     nameC: string;
-    nameD: string;
+    nameD?: string;
     priceA: string;
     priceB: string;
     priceC: string;
-    priceD: string;
+    priceD?: string;
     detail: string;
     image: any;
   };
@@ -37,10 +43,10 @@ const CardItems = () => {
       nameB: "クラシックカットコース",
       nameC: "高校生カットコース",
       nameD: "高校生エステ無しコース",
-      priceA: "¥6,500",
-      priceB: "¥4,500",
-      priceC: "¥4,000",
-      priceD: "¥3,500",
+      priceA: "¥7,150",
+      priceB: "¥4,950",
+      priceC: "¥4,400",
+      priceD: "¥3,850",
       detail: "※シャンプー、ブロー込み",
       image: cutImage,
     },
@@ -57,6 +63,61 @@ const CardItems = () => {
       priceD: "¥6,500~",
       detail: "※上記はショート料金となります",
       image: colorImage,
+    },
+    {
+      id: 3,
+      title: "PERM",
+      nameA: "ナノミストパーマ",
+      nameB: "ケアアイロンパーマ",
+      nameC: "オプショナルクリープ",
+      nameD: "縮毛矯正",
+      priceA: "¥5,500~",
+      priceB: "¥5,500~",
+      priceC: "¥9,350~",
+      priceD: "¥11,000~",
+      detail: "※上記はショート料金となります",
+      image: permImage,
+    },
+    {
+      id: 4,
+      title: "OTHER",
+      nameA: "鼻毛バイバイエステ",
+      nameB: "イヤースッキリ",
+      nameC: "北斗のマッサージ",
+      nameD: "シャイニングクロー",
+      priceA: "¥770",
+      priceB: "¥550",
+      priceC: "¥550",
+      priceD: "¥550",
+      detail: "※詳しくはスタッフまでお尋ねください",
+      image: otherImage,
+    },
+    {
+      id: 5,
+      title: "HEAD SPA",
+      nameA: "クレンジングスパ",
+      nameB: "育毛ヘッドスパ",
+      nameC: "育毛オプショナルスパ",
+      nameD: " ",
+      priceA: "¥1,650",
+      priceB: "¥3,850",
+      priceC: "¥4,620",
+      priceD: " ",
+      detail: "※詳しくはスタッフまでお尋ねください",
+      image: headspaImage,
+    },
+    {
+      id: 6,
+      title: "SHAVING",
+      nameA: "フェイスケアシェープ",
+      nameB: "タイムスリップエステ",
+      nameC: "各種パック",
+      priceA: "¥2,530",
+      priceB: "¥1,100",
+      priceC: "¥1,100",
+      priceD: "",
+      detail: "※詳しくはスタッフまでお尋ねください",
+      image: shavingImage,
     },
   ];
 
