05b09475d561a47e3a86771be2ac651d52170c77
Glimmer5 <yu_shimizu2@icloud.com>
2023-02-22 14:14:33 +0900

    [update]menu

--- src/pages/menu.tsx
+++ src/pages/menu.tsx
@@ -1,8 +1,16 @@
-import { Container, Box } from "@chakra-ui/react";
+import { Container, Box, Text, Divider } from "@chakra-ui/react";
 import Head from "next/head";
 import MotionLayout from "../components/motionLayout";
 import BreadCrumbButton from "../components/breadCrumbButton";
-import CardItems from '../components/cardItems';
+import CardItems from "../components/cardItems";
+import Title from "../components/title";
+import ArticleBox from "../components/articleBox";
+
+import { FaCcVisa } from "react-icons/fa";
+import { FaCcMastercard } from "react-icons/fa";
+import { SiAmericanexpress } from "react-icons/si";
+import { FaCcJcb } from "react-icons/fa";
+import { BrandPaypay } from "tabler-icons-react";
 
 const Menu: React.FC = () => {
   return (
@@ -17,6 +25,55 @@ const Menu: React.FC = () => {
           </Box>
           <Box mb={10}>
             <CardItems />
+          </Box>
+          <Box mb={3}>
+            <Title>決済方法</Title>
+          </Box>
+          <Box mb={10}>
+            <ArticleBox>
+              <Text mb={2}>クレジットカード</Text>
+              <Box
+                display="flex"
+                justifyContent="space-between"
+                flexWrap="wrap"
+                gap="0.5em"
+              >
+                <Box display="flex">
+                  <Box pr={1}>
+                    <FaCcVisa size={25} />
+                  </Box>
+                  <Text>VISA</Text>
+                </Box>
+                <Box display="flex">
+                  <Box pr={1}>
+                    <FaCcMastercard size={25} />
+                  </Box>
+                  <Text>MasterCard</Text>
+                </Box>
+                <Box display="flex">
+                  <Box pr={1}>
+                    <FaCcJcb size={25} />
+                  </Box>
+                  <Text>JCB</Text>
+                </Box>
+                <Box display="flex">
+                  <Box pr={1}>
+                    <SiAmericanexpress size={25} />
+                  </Box>
+                  <Text>AmericanExpress</Text>
+                </Box>
+                <Divider my={2} size="xl" />
+              </Box>
+              <Box display="flex" justifyContent="space-between">
+                <Text>QRコード決済</Text>
+                <Box display="flex">
+                  <Box pr={1}>
+                    <BrandPaypay size={25} />
+                  </Box>
+                  <Text>PayPay</Text>
+                </Box>
+              </Box>
+            </ArticleBox>
           </Box>
         </Box>
       </Container>
